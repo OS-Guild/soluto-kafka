f30c6b52ee3d4789b8b985ab7501e01ecbb5dddb
Guy Segal <guy@soluto.com>
2019-02-12 23:08:40 +0200

    trying to fix crash

--- src/main/java/ConsumerCreator.java
+++ src/main/java/ConsumerCreator.java
@@ -21,7 +21,7 @@ class ConsumerCreator {
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("enable.auto.commit", "false");
-        props.put("max.poll.interval.ms", String.valueOf(config.POLL_INTERVAL));
+        props.put("max.poll.interval.ms", String.valueOf(Integer.MAX_VALUE));
         props.put("max.poll.records", String.valueOf(config.POLL_RECORDS));
         props.put("session.timeout.ms", "250000");
 

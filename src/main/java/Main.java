f120f9b551b8ad282ae18798920af34dcef8bedf
Guy Segal <guy@soluto.com>
2019-02-12 20:30:53 +0200

    added shutdownHook (SIGTERM)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class Main {
     public static void main(String[] args) {
+
         Config config;
         try {
             config = new Config();
@@ -21,20 +22,25 @@ public class Main {
         KafkaConsumer<String, String> consumer = ConsumerCreator.create(config);
         consumer.subscribe(Collections.singletonList(config.TOPIC));
 
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                consumer.unsubscribe();
+                consumer.close();
+            }
+        });
+
         while (true) {
             ExecutorService executor = Executors.newFixedThreadPool(config.CONCURRENCY);
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
 
             System.out.println(records.count());
 
-            Iterable<ConsumerRecord<String, String>> consumerRecords = config.SHOULD_DEDUP_BY_KEY ?
-                    StreamSupport.stream(records.spliterator(), false)
-                            .collect(Collectors.groupingBy(ConsumerRecord::key))
-                            .entrySet()
-                            .stream()
-                            .map(x -> x.getValue().get(0))
-                            .collect(Collectors.toList()) :
-                    records;
+            Iterable<ConsumerRecord<String, String>> consumerRecords = config.SHOULD_DEDUP_BY_KEY
+                    ? StreamSupport.stream(records.spliterator(), false)
+                            .collect(Collectors.groupingBy(ConsumerRecord::key)).entrySet().stream()
+                            .map(x -> x.getValue().get(0)).collect(Collectors.toList())
+                    : records;
 
             for (ConsumerRecord<String, String> record : consumerRecords) {
                 executor.submit(new ConsumerRecordRunnable(record, config));
@@ -43,7 +49,8 @@ public class Main {
             executor.shutdown();
 
             while (true) {
-                if (executor.isTerminated()) break;
+                if (executor.isTerminated())
+                    break;
             }
 
             consumer.commitSync();

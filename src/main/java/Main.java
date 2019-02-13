b4d3b0f4fb35b27aa4aa0d284e53bbf3ed6d255c
Guy Segal <guy@soluto.com>
2019-02-13 11:33:51 +0200

    CommitFailedException can happen. And it’s OK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import org.apache.kafka.clients.consumer.CommitFailedException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -21,11 +22,10 @@ public class Main {
         KafkaConsumer<String, String> consumer = ConsumerCreator.create(config);
         consumer.subscribe(Collections.singletonList(config.TOPIC));
 
-        Runtime.getRuntime()
-                .addShutdownHook(new Thread(() -> {
-                    consumer.unsubscribe();
-                    consumer.close();
-                }));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            consumer.unsubscribe();
+            consumer.close();
+        }));
 
         while (true) {
             ExecutorService executor = Executors.newFixedThreadPool(config.CONCURRENCY);
@@ -35,11 +35,8 @@ public class Main {
 
             Iterable<ConsumerRecord<String, String>> consumerRecords = config.SHOULD_DEDUP_BY_KEY
                     ? StreamSupport.stream(records.spliterator(), false)
-                    .collect(Collectors.groupingBy(ConsumerRecord::key))
-                    .entrySet()
-                    .stream()
-                    .map(x -> x.getValue().get(0))
-                    .collect(Collectors.toList())
+                            .collect(Collectors.groupingBy(ConsumerRecord::key)).entrySet().stream()
+                            .map(x -> x.getValue().get(0)).collect(Collectors.toList())
                     : records;
 
             for (ConsumerRecord<String, String> record : consumerRecords) {
@@ -52,8 +49,11 @@ public class Main {
                 if (executor.isTerminated())
                     break;
             }
+            try {
+                consumer.commitSync();
+            } catch (CommitFailedException e) {
 
-             consumer.commitSync();
+            }
         }
     }
 }

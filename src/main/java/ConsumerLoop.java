7aea5e6a8ec56ed671f9b49a2236761852c98dba
Noam Okman <noamokman@gmail.com>
2019-05-06 15:03:20 +0300

    Add support for partitions

--- src/main/java/ConsumerLoop.java
+++ src/main/java/ConsumerLoop.java
@@ -7,7 +7,9 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -46,17 +48,17 @@ public class ConsumerLoop implements Runnable, IReady {
             while (running) {
                 Thread.sleep(Config.PROCESSING_DELAY);
                 var consumed = consumer.poll(Duration.ofMillis(Config.CONSUMER_POLL_TIMEOUT));
-                if (ready == false && consumer.assignment().size() > 0) {
+                if (!ready && consumer.assignment().size() > 0) {
                     ready = true;
                     Monitor.consumerReady(id);
-                }                
+                }
                 if (consumed.count() == 0) continue;
                 Monitor.consumed(consumed);
-                
-                var consumedDedup = dedup(consumed);
-                Monitor.consumedDedup(consumed);
-                
-                process(consumedDedup);
+
+                var consumedPartitioned = partitionByKey(consumed);
+                Monitor.consumedPartitioned(consumedPartitioned);
+
+                process(consumedPartitioned);
 
                 try {
                     consumer.commitSync();
@@ -67,8 +69,7 @@ public class ConsumerLoop implements Runnable, IReady {
         } catch (Exception e) {
             if (e.getCause() instanceof ConnectException) {
                 Monitor.targetConnectionUnavailable();
-            }
-            else {
+            } else {
                 Monitor.unexpectedError(e);
             }
         } finally {
@@ -83,73 +84,80 @@ public class ConsumerLoop implements Runnable, IReady {
 
     @Override
     public boolean ready() {
-		return ready;
-	}
-
-    private Iterable<ConsumerRecord<String, String>> dedup(ConsumerRecords<String, String> records) {
-        return Config.SHOULD_DEDUP_BY_KEY
-            ? StreamSupport.stream(records.spliterator(), false)
-                .collect(Collectors.groupingBy(ConsumerRecord::key))
-                .entrySet()
-                .stream()
-                .map(x -> x.getValue().get(0))
-                .collect(Collectors.toList())
-            : records;
+        return ready;
     }
 
-    private void process(Iterable<ConsumerRecord<String, String>> records) throws IOException, InterruptedException {
-        Flowable.fromIterable(records)
-            .doOnNext(record -> Monitor.messageLatency(record))
-            .flatMap(record -> 
-                Flowable.fromFuture(callTarget(record)), Config.CONCURRENCY)
-                    .flatMap(x -> {
-                        if (x.type == TargetResponseType.Error) {
-                            return Flowable.error(x.exception.getCause());
-                        }
-                        return Flowable.empty();
-                    })
-            .subscribeOn(Schedulers.io())
-            .blockingSubscribe();
+    private Iterable<Iterable<ConsumerRecord<String, String>>> partitionByKey(ConsumerRecords<String, String> records) {
+        return StreamSupport.stream(records.spliterator(), false)
+                .collect(Collectors.groupingBy(ConsumerRecord::key))
+                .values()
+                .stream()
+                .map(consumerRecords -> {
+                    List<ConsumerRecord<String, String>> sorted = consumerRecords
+                            .stream()
+                            .sorted(Comparator.comparingLong(ConsumerRecord::offset))
+                            .collect(Collectors.toList());
+
+                    return Config.SHOULD_DEDUP_BY_KEY ? Collections.singletonList(sorted.get(0)) :  sorted;
+                })
+                .collect(Collectors.toList());
+    }
+
+    private void process(Iterable<Iterable<ConsumerRecord<String, String>>> partitions) throws IOException, InterruptedException {
+        Flowable.fromIterable(partitions)
+                .flatMap(this::processPartition, Config.CONCURRENCY)
+                .subscribeOn(Schedulers.io())
+                .blockingSubscribe();
+    }
+
+    private Flowable processPartition(Iterable<ConsumerRecord<String, String>> partition) {
+        return Flowable.fromIterable(partition)
+                .doOnNext(Monitor::messageLatency)
+                .flatMap(record -> Flowable.fromFuture(callTarget(record)))
+                .flatMap(x -> x.type == TargetResponseType.Error ? Flowable.error(x.exception.getCause()) : Flowable.empty());
     }
 
     private CompletableFuture<TargetResponse> callTarget(ConsumerRecord<String, String> record) {
         var request = HttpRequest
-            .newBuilder()
-            .uri(URI.create(Config.TARGET_ENDPOINT))
-            .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString(record.value()))
-            .build();
+                .newBuilder()
+                .uri(URI.create(Config.TARGET_ENDPOINT))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(record.value()))
+                .build();
 
         var executionStart = new Date().getTime();
 
-         var retryPolicy = new RetryPolicy<HttpResponse<String>>()
-            .withBackoff(10, 250, ChronoUnit.MILLIS, 5)            
-            .abortOn(ConnectException.class)
-            .handleResultIf(r -> r.statusCode() >= 500)
-            .onSuccess(x -> {
-                var statusCode = x.getResult().statusCode();
-                if (400 <= statusCode && statusCode < 500) {
-                    produce("poison", Config.POISON_MESSAGE_TOPIC, record);
-                } else {
+        var retryPolicy = new RetryPolicy<HttpResponse<String>>()
+                .withBackoff(10, 250, ChronoUnit.MILLIS, 5)
+                .abortOn(ConnectException.class)
+                .handleResultIf(r -> r.statusCode() >= 500)
+                .onSuccess(x -> {
+                    var statusCode = x.getResult().statusCode();
+
+                    if (400 <= statusCode && statusCode < 500) {
+                        produce("poison", Config.POISON_MESSAGE_TOPIC, record);
+                        return;
+                    }
+
                     Monitor.processCompleted(executionStart);
-                }
-            })            
-            .onFailedAttempt(x -> Monitor.targetExecutionRetry(record, x.getLastResult(),  x.getLastFailure(), x.getAttemptCount()))
-            .onRetriesExceeded(__ -> produce("retry", Config.RETRY_TOPIC, record));
+                })
+                .onFailedAttempt(x -> Monitor.targetExecutionRetry(record, x.getLastResult(), x.getLastFailure(), x.getAttemptCount()))
+                .onRetriesExceeded(__ -> produce("retry", Config.RETRY_TOPIC, record));
 
         return Failsafe
-            .with(retryPolicy)
-            .getStageAsync(() -> client.sendAsync(request, HttpResponse.BodyHandlers.ofString()))
-            .thenApplyAsync(__ ->  TargetResponse.Success)
-            .exceptionally(throwable -> TargetResponse.Error(throwable));
+                .with(retryPolicy)
+                .getStageAsync(() -> client.sendAsync(request, HttpResponse.BodyHandlers.ofString()))
+                .thenApplyAsync(__ -> TargetResponse.Success)
+                .exceptionally(TargetResponse::Error);
     }
 
     private void produce(String topicPrefix, String topic, ConsumerRecord<String, String> record) {
-        producer.send(new ProducerRecord<>(topic, record.key().toString(), record.value().toString()), (metadata, err) -> {
+        producer.send(new ProducerRecord<>(topic, record.key(), record.value()), (metadata, err) -> {
             if (err != null) {
-                Monitor.produceError(topicPrefix, record ,err);
+                Monitor.produceError(topicPrefix, record, err);
                 return;
             }
+
             Monitor.topicProduced(topicPrefix, record);
         });
     }

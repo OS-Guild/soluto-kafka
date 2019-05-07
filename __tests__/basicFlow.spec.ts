cccf0d5f5041d3fe97d45a4a9ecde57f9b69160b
Noam Okman <noamokman@gmail.com>
2019-05-07 17:25:06 +0300

    Basic tests

--- __tests__/basicFlow.spec.ts
+++ __tests__/basicFlow.spec.ts
@@ -1,5 +1,5 @@
-import fetch from 'node-fetch';
 import delay from 'delay';
+import * as got from 'got';
 
 jest.setTimeout(40000);
 
@@ -7,13 +7,40 @@ describe('basic flow', () => {
     it('services are alive', async () => {
         try {
             await delay(30000);
-            await fetch('http://localhost:2000/isAlive'); // consumer
-            await fetch('http://localhost:2500/isAlive'); // producer
+            await got('http://localhost:2000/isAlive'); // consumer
+            await got('http://localhost:2500/isAlive'); // producer
         } catch (e) {
             console.log(e.text ? await e.text() : e);
             throw e;
         }
     });
 
-    describe('logic', () => {});
+    describe('logic', () => {
+        beforeAll(() => got.post('http://localhost:3000/fake_server_admin/clear'));
+        it('should produce', async () => {
+            await got.post('http://localhost:2500/produce', {json: true, body: [{key: 'key', message: {data: 1}}]});
+        });
+
+        it('should consume', async () => {
+            const {
+                body: {callId},
+            } = await got.post('http://localhost:3000/fake_server_admin/calls', {
+                json: true,
+                body: {
+                    method: 'post',
+                    url: '/target',
+                    statusCode: 200,
+                },
+            });
+            await got.post('http://localhost:2500/produce', {json: true, body: [{key: 'key', message: {data: 1}}]});
+
+            await delay(500);
+
+            const {
+                body: {hasBeenMade},
+            } = await got(`http://localhost:3000/fake_server_admin/calls?callId=${callId}`, {json: true});
+
+            expect(hasBeenMade).toBeTruthy();
+        });
+    });
 });

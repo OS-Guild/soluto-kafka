f4ef70f55ee71b95ddccc15a67a1fbaa2788b717
guy eagelberg <guye@soluto.com>
2019-06-18 11:30:17 +0300

    add logs for memory init

--- kafka-producer-java/src/main/java/Main.java
+++ kafka-producer-java/src/main/java/Main.java
@@ -8,6 +8,12 @@ public class Main {
         init();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> close()));
         monitor.serviceStarted();
+
+        int processors = runtime.availableProcessors();
+        long maxMemory = runtime.maxMemory();
+
+        System.out.format("Number of processors: %d\n", processors);
+        System.out.format("Max memory: %d bytes\n", maxMemory);
     }
 
     private static void init() throws Exception {

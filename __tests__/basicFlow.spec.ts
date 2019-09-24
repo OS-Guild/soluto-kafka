a8a04a6d3772291cef817bd329f3d1076392e597
Guy Segal <guy@soluto.com>
2019-09-24 12:06:02 +0300

    .

--- __tests__/basicFlow.spec.ts
+++ __tests__/basicFlow.spec.ts
@@ -5,8 +5,12 @@ import fetch from 'node-fetch';
 jest.setTimeout(60000);
 
 describe('basic flow', () => {
+    beforeAll(async () => {
+        await delay(30000)
+        beforeAll(() => got.post('http://localhost:3000/fake_server_admin/clear'));
+
+    })
     it('services are alive', async () => {
-        console.log('waiting for kafka to be ready..');
         let attempts = 3;
         while (attempts > 0) {
             try {
@@ -24,33 +28,29 @@ describe('basic flow', () => {
         fail();
     });
 
-    describe('logic', () => {
-        beforeAll(() => got.post('http://localhost:3000/fake_server_admin/clear'));
-
-        it('should produce and consume', async () => {
-            const {
-                body: {callId},
-            } = await got.post('http://localhost:3000/fake_server_admin/calls', {
-                json: true,
-                body: {
-                    method: 'post',
-                    url: '/target',
-                    statusCode: 200,
-                },
-            });
-            await fetch('http://localhost:2500/produce', {
+    it('should produce and consume', async () => {
+        const {
+            body: {callId},
+        } = await got.post('http://localhost:3000/fake_server_admin/calls', {
+            json: true,
+            body: {
                 method: 'post',
-                body: JSON.stringify([{key: 'key', message: {data: 1}}]),
-                headers: {'Content-Type': 'application/json'},
-            });
-
-            await delay(5000);
-
-            const {
-                body: {hasBeenMade},
-            } = await got(`http://localhost:3000/fake_server_admin/calls?callId=${callId}`, {json: true});
-
-            expect(hasBeenMade).toBeTruthy();
+                url: '/target',
+                statusCode: 200,
+            },
         });
+        await fetch('http://localhost:2500/produce', {
+            method: 'post',
+            body: JSON.stringify([{key: 'key', message: {data: 1}}]),
+            headers: {'Content-Type': 'application/json'},
+        });
+
+        await delay(5000);
+
+        const {
+            body: {hasBeenMade},
+        } = await got(`http://localhost:3000/fake_server_admin/calls?callId=${callId}`, {json: true});
+
+        expect(hasBeenMade).toBeTruthy();
     });
 });

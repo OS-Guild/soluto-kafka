09ea95d75336ca8722529d76a79fccc07a057e06
guysegal <guy@guys-mac.local>
2020-02-04 20:00:21 +0200

    read secret from separated files

--- kafka-producer-java/src/main/java/Config.java
+++ kafka-producer-java/src/main/java/Config.java
@@ -4,8 +4,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 class Config {
     //Required
@@ -47,36 +45,32 @@ class Config {
         LINGER_TIME_MS = getOptionalInt(dotenv, "LINGER_TIME_MS", 0);
         COMPRESSION_TYPE = getOptionalString(dotenv, "COMPRESSION_TYPE", "none");
 
-        JSONObject secrets = buildSecrets(dotenv);
-
-        TRUSTSTORE_LOCATION = "client.truststore.jks";
-        KEYSTORE_LOCATION = "client.keystore.p12";
-
-        String truststore = getOptionalSecret(secrets, dotenv, "TRUSTSTORE", null);
-        if (truststore != null) {
-            writeToFile(TRUSTSTORE_LOCATION, truststore);
-            TRUSTSTORE_PASSWORD = getSecret(secrets, dotenv, "TRUSTSTORE_PASSWORD");
+        String truststoreFilePath = getOptionalString(dotenv, "TRUSTSTORE_FILE_PATH", null);
+        if (truststoreFilePath != null) {
+            TRUSTSTORE_LOCATION = "client.truststore.jks";
+            writeToFile(TRUSTSTORE_LOCATION, readSecretFromFile(getString(dotenv, "TRUSTSTORE_FILE_PATH")));
+            TRUSTSTORE_PASSWORD = readSecretFromFile(getString(dotenv, "TRUSTSTORE_PASSWORD_FILE_PATH"));
         }
 
         SECURITY_PROTOCOL = getOptionalString(dotenv, "SECURITY_PROTOCOL", "");
 
         if (SECURITY_PROTOCOL.equals("SSL")) {
-            KEYSTORE_PASSWORD = getSecret(secrets, dotenv, "KEYSTORE_PASSWORD");
-            KEY_PASSWORD = getSecret(secrets, dotenv, "KEY_PASSWORD");
-            String keystore = getSecret(secrets, dotenv, "KEYSTORE");
-            writeToFile(KEYSTORE_LOCATION, keystore);
+            KEYSTORE_LOCATION = "client.keystore.p12";
+            KEYSTORE_PASSWORD = readSecretFromFile(getString(dotenv, "KEYSTORE_PASSWORD_FILE_PATH"));
+            writeToFile(KEYSTORE_LOCATION, readSecretFromFile(getString(dotenv, "KEYSTORE_FILE_PATH")));
+            KEY_PASSWORD = readSecretFromFile(getString(dotenv, "KEY_PASSWORD_FILE_PATH"));
             AUTHENTICATED_KAFKA = true;
         }
 
         if (SECURITY_PROTOCOL.equals("SASL_SSL")) {
             SASL_USERNAME = getString(dotenv, "SASL_USERNAME");
-            SASL_PASSWORD = getSecret(secrets, dotenv, "SASL_PASSWORD");
+            SASL_PASSWORD = readSecretFromFile(getString(dotenv, "SASL_PASSWORD_FILE_PATH"));
             AUTHENTICATED_KAFKA = true;
         }
 
         STATSD_PRODUCER_NAME = getOptionalString(dotenv, "STATSD_PRODUCER_NAME", null);
         if (STATSD_PRODUCER_NAME != null) {
-            STATSD_API_KEY = getSecret(secrets, dotenv, "STATSD_API_KEY");
+            STATSD_API_KEY = readSecretFromFile(getString(dotenv, "STATSD_API_KEY_FILE_PATH"));
             STATSD_ROOT = getString(dotenv, "STATSD_ROOT");
             STATSD_HOST = getString(dotenv, "STATSD_HOST");
             STATSD_CONFIGURED = true;
@@ -87,46 +81,8 @@ class Config {
         Files.write(Paths.get(path), Base64.getDecoder().decode(value.getBytes(StandardCharsets.UTF_8)));
     }
 
-    private static JSONObject buildSecrets(Dotenv dotenv) {
-        return readSecrets(getOptionalString(dotenv, "SECRETS_FILE_LOCATION", null));
-    }
-
-    private static JSONObject readSecrets(String secretsFileLocation) {
-        if (secretsFileLocation == null) {
-            return new JSONObject();
-        }
-
-        try {
-            return new JSONObject(new String(Files.readAllBytes(Paths.get(secretsFileLocation))));
-        } catch (IOException e) {
-            return new JSONObject();
-        }
-    }
-
-    private static String getSecret(JSONObject secrets, Dotenv dotenv, String name) throws Exception {
-        String value = dotenv.get(name);
-
-        if (value != null) {
-            return value;
-        }
-
-        String secret = null;
-
-        try {
-            secret = secrets.getString(name);
-        } catch (JSONException ignored) {
-            throw new Exception("missing env var: " + name);
-        }
-
-        return secret;
-    }
-
-    private static String getOptionalSecret(JSONObject secrets, Dotenv dotenv, String name, String fallback) {
-        try {
-            return getSecret(secrets, dotenv, name);
-        } catch (Exception e) {
-            return fallback;
-        }
+    private static String readSecretFromFile(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 
     private static String getString(Dotenv dotenv, String name) throws Exception {

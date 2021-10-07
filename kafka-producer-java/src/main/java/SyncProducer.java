62b91bdf7422100dd372cbd3eea07a58d87a362d
yoavsh <yoavsh@soluto.com>
2021-10-07 11:36:40 +0300

    remove comment

--- kafka-producer-java/src/main/java/SyncProducer.java
+++ kafka-producer-java/src/main/java/SyncProducer.java
@@ -16,8 +16,6 @@ public class SyncProducer extends AbstractProducer {
             ready = true;
         } catch (InterruptedException | ExecutionException e) {
             Monitor.produceError(e);
-
-            //            ready = false;
             throw e;
         }
     }
